741b58c0878913b67ae38f1cd5187b0fb72e9ccf
Dane Petersen <dane.m.petersen@gmail.com>
2021-06-20 09:30:01 -0500

    completed question 18

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -104,4 +104,5 @@ switch (true) {
 console.log(mostPets);
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
-
+result = adventurous == true ? "Adventures are great!" : "How about we stay home?";
+console.log(result);
